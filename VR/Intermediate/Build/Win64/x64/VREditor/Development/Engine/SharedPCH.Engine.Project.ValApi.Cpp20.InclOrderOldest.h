098e3671cfcd778bd7f2ddc0abe6713ca74a913f
TylerNunes <22524686@stu.mmu.ac.uk>
2024-11-11 16:02:03 +0000

    VRExpansionPluginInstall
    Installed the vr expansion plugin to the project

---
+++ VR/Intermediate/Build/Win64/x64/VREditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.InclOrderOldest.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "C:/Users/Tyler Nunes/Documents/GitHub/TheBestTeam/VR/Intermediate/Build/Win64/x64/VREditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.InclOrderOldest.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
---
+++ VR/Intermediate/Build/Win64/x64/VREditor/Development/Slate/SharedPCH.Slate.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.38.33141
+#include "C:/Users/Tyler Nunes/Documents/GitHub/TheBestTeam/VR/Intermediate/Build/Win64/x64/VREditor/Development/Slate/SharedPCH.Slate.Cpp20.h"
