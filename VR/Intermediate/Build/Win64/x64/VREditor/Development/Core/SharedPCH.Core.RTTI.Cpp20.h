8565acbb60712f25bae67fce29438f4c26d2849c
TylerNunes <22524686@stu.mmu.ac.uk>
2024-11-11 18:44:05 +0000

    VRExpansionPluginInstallation
    Installed Vr Expansion Plugin
    Updated Project to c++ and blueprints(needed for the vrplugin)

---
+++ VR/Intermediate/Build/Win64/x64/VREditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Core/Public/CoreSharedPCH.h
+#include "C:/Users/Tyler Nunes/Documents/GitHub/TheBestTeam/VR/Intermediate/Build/Win64/x64/VREditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
+#include "Runtime/Core/Public/CoreSharedPCH.h"
